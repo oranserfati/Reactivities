8ea57a07a2f579437c1272b8e76bd5698c44169f
oranserfati <Oranserfati@gmail.com>
2024-09-21 20:37:04 +0300

    End of section 5

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,8 +1,8 @@
 import 'semantic-ui-css/semantic.min.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from './app/layouts/App.tsx'
+import './app/layouts/styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
