33b98878ce485ec194b30bda3514d7451309e613
oranserfati <Oranserfati@gmail.com>
2024-09-30 21:55:00 +0300

    End of section 13

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,7 +1,12 @@
 import { Link } from "react-router-dom";
 import { Button, Container, Header, Image, Segment } from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
+import { observer } from "mobx-react-lite";
+import LoginForm from "../users/LoginForm";
+import RegisterForm from "../users/registerForm";
 
-export default function homePage() {
+export default observer(function homePage() {
+    const { userStore, modalStore } = useStore();
     return (
         <Segment inverted textAlign="center" vertical className="masthead">
             <Container text>
@@ -9,11 +14,26 @@ export default function homePage() {
                     <Image size='massive' src='/assets/logo.png' alt='logo' style={{ marginBottom: 12 }} />
                     Reactivites
                 </Header>
-                <Header as='h2' inverted content='Welcome to Reactivities' />
-                <Button as={Link} to='/activities' size='huge' inverted>
-                    Take me to the Activities!
-                </Button>
+                {userStore.isLoggedIn ? (
+                    <>
+                        <Header as='h2' inverted content='Welcome to Reactivities' />
+                        <Button as={Link} to='/activities' size='huge' inverted>
+                            Go to Activities
+                        </Button>
+                    </>
+
+                ) : (
+                    <>
+                        <Button onClick={() => modalStore.openModal(<LoginForm />)} size='huge' inverted>
+                            Login
+                        </Button>
+                        <Button onClick={() => modalStore.openModal(<RegisterForm />)} size='huge' inverted>
+                            Register
+                        </Button>
+                    </>
+
+                )}
             </Container>
         </Segment>
     )
-}
+})
